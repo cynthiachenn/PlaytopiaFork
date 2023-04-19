69c11de0cb39d4370bac78a74af0f5fe4d7f19e3
cynthiachenn <121204990+cynthiachenn@users.noreply.github.com>
2023-04-19 14:41:15 -0400

    Add files via upload

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -1,4 +1,4 @@
-# Some set up for the application 
+# Some set up for the application
 
 from flask import Flask
 from flaskext.mysql import MySQL
@@ -8,22 +8,22 @@ db = MySQL()
 
 def create_app():
     app = Flask(__name__)
-    
-    # secret key that will be used for securely signing the session 
-    # cookie and can be used for any other security related needs by 
+   
+    # secret key that will be used for securely signing the session
+    # cookie and can be used for any other security related needs by
     # extensions or your application
     app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
 
-    # these are for the DB object to be able to connect to MySQL. 
+    # these are for the DB object to be able to connect to MySQL.
     app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123' #open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'PlaytopiaDB'  # Change this to your DB name
 
-    # Initialize the database object with the settings above. 
+    # Initialize the database object with the settings above.
     db.init_app(app)
-    
+   
     # Add a default route
     @app.route("/")
     def welcome():
@@ -34,9 +34,14 @@ def create_app():
     from src.customers.customers import customers
     from src.products.products  import products
 
+    from src.games.games import games
+    from src.orders.orders import orders
+
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(games,       url_prefix='/g')
+    app.register_blueprint(orders,      url_prefix='/o')
 
     return app
